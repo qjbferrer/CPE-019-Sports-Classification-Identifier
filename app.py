a4745cc6d0f8192c4c49a8a659ae042c164f3d3e
Joseph Bryan Ferrer <143077202+qjbferrer@users.noreply.github.com>
2024-05-15 21:15:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,5 @@ from PIL import Image
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('final_model.h5')
+  model=tf.keras.models.load_model('deploy_cnn.best.hdf5')
   return model
-model=load_model()
-st.write("""
-# Sports Image Classification"""
-)
-file=st.file_uploader("Predict the sport that is being represented in the image.",type=["jpg","png"])
