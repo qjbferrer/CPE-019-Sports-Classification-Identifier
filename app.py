e4f75a08233cd238f6ef3bee9d86b2ea412d234b
Joseph Bryan Ferrer <143077202+qjbferrer@users.noreply.github.com>
2024-05-15 21:10:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,29 +15,24 @@ def load_keras_model():
     return model
 
 # Load the model
-with st.spinner('Model is being loaded..'):
-    model = load_keras_model()
+model = load_keras_model()
 
 # Function for form and prediction
 def import_and_predict(image, model):
     label = predict_label(image, model)
     return label
 
-# Display the prediction result
-def display_prediction_result(image, label):
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-    st.markdown(f"<h2 style='text-align: center;'>{label}</h2>", unsafe_allow_html=True)
-
 # Form for image upload and prediction
-with st.form("image_form"):
-    uploaded_file = st.file_uploader("Upload an image of a sport being played:", type=["jpg", "jpeg", "png"])
+with st.form("my_form"):
+    uploaded_file = st.file_uploader("Upload an image of a sport being played:", type="jpg")
     submitted = st.form_submit_button("Submit")
     
     if submitted:
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
             label = import_and_predict(image, model)
-            display_prediction_result(image, label)
+            st.image(image, caption="Uploaded Image", use_column_width=True)
+            st.markdown(f"<h2 style='text-align: center;'>{label}</h2>", unsafe_allow_html=True)
         else:
             st.write("Please upload an image or choose a sample image.")
 
@@ -46,8 +41,7 @@ st.write("If you would not like to upload an image, you can use the sample image
 sample_img_choice = st.button("Use Sample Image")
 
 if sample_img_choice:
-    sample_image_path = "test_cricket.jpg"
-    image = Image.open(sample_image_path)
+    image = Image.open("test_cricket.jpg")
     st.image(image, caption="Sample Image", use_column_width=True)
     label = import_and_predict(image, model)
-    display_prediction_result(image, label)
+    st.markdown(f"<h2 style='text-align: center;'>{label}</h2>", unsafe_allow_html=True)
