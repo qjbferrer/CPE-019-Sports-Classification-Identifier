cd0dfa3fc90d4c22699ddb5aa29b900a42ea3d6a
Joseph Bryan Ferrer <143077202+qjbferrer@users.noreply.github.com>
2024-05-15 21:13:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,15 @@ from tensorflow.keras.models import load_model
 from utils import predict_label
 from PIL import Image
 
-# Title and description
-st.title("Sports Image Classification")
-st.write("Predict the sport that is being represented in the image.")
-
-# Load Keras model with caching
 @st.cache(allow_output_mutation=True)
-def load_keras_model():
-    model = load_model('final_model.h5')
-    return model
-
-# Load the model
-model = load_keras_model()
+def load_model():
+  model=tf.keras.models.load_model('final_model.hdf5')
+  return model
+model=load_model()
+st.write("""
+# Sports Image Classification"""
+)
+file=st.file_uploader("Predict the sport that is being represented in the image.",type=["jpg","png"])
 
 # Function for form and prediction
 def import_and_predict(image, model):
